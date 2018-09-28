46a9cd00dac9bb0c44fe9817e419c1c5eab9004d
todun <todun@users.noreply.github.com>
2018-09-28 05:29:55 +0000

    7.5  Update data container to return incompleteCount

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -85,5 +85,6 @@ class App extends Component {
 export default withTracker(() => {
     return {
         tasks: Tasks.find({}, { sort: { createdAt: -1 } }).fetch(),
+        incompleteCount: Tasks.find({ checked: { $ne: true } }).count(),
     };
 })(App);
