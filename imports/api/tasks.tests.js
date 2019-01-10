afdb3dd48665d5ee1f1105b400bce78136da010c
todun <todun@users.noreply.github.com>
2019-01-10 11:55:56 +0000

    add can insert task

--- imports/api/tasks.tests.js
+++ imports/api/tasks.tests.js
@@ -3,14 +3,30 @@
 import { Meteor } from 'meteor/meteor';
 import { Random } from 'meteor/random';
 import { assert } from 'chai';
+import { Accounts } from "meteor/accounts-base";
 
 import { Tasks } from './tasks.js';
 
 if (Meteor.isServer) {
     describe('Tasks', () => {
         describe('methods', () => {
-            const userId = Random.id();
-            let taskId;
+            // const userId = Random.id();
+            const username = 'me';
+            let taskId, userId;
+
+            before(() => {
+                // Create user if not already created
+                const user = Meteor.users.findOne({username: username});
+                if (!user) {
+                    userId = Accounts.createUser({
+                        'username': username,
+                        'email': username + '@email.org',
+                        'password' : username+ 'password'
+                    })
+                } else {
+                    userId = user._id;
+                }
+            });
 
             beforeEach(() => {
                 Tasks.remove({});
@@ -58,6 +74,27 @@ if (Meteor.isServer) {
 
                 // Verify that task is not deleted
                 assert.equal(Tasks.find().count(), 1);
+            });
+
+            it("can insert task", () => {
+                // 1. housekeeping/set up environment
+                const taskOneText = 'task one';
+
+                // 2. get method unit
+                const insertTask = Meteor.server.method_handlers['tasks.insert'];
+
+                // 3. Setup fake global object
+                // create fake user object
+                let fakeUserObject = {
+                    'userId': userId,
+                    'username': username
+                };
+
+                // 4. Run method with fake object and required arguments
+                insertTask.apply(fakeUserObject, [taskOneText]);
+
+                // 5. Perform checks
+                assert.equal(Tasks.find().count(), 2);
             });
         });
     });
